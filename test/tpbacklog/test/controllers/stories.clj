1ea689691870556ff2d8841cf6b3ced4743105de
tiredpixel <tp@tiredpixel.com>
2014-05-25 00:05:51 +0100

    Refactor test.controllers.stories , deduplicating.

--- test/tpbacklog/test/controllers/stories.clj
+++ test/tpbacklog/test/controllers/stories.clj
@@ -17,6 +17,10 @@
   (let [response (app (request :post "/stories" STORY_VALID))]
     ((response :headers) "Location")))
 
+(defn- test-r-create-invalid [story]
+  (let [response (app (request :post "/stories" story))]
+    (is (= (response :status) 400))))
+
 (deftest test-r-create
   (testing "valid"
     (let [story STORY_VALID
@@ -24,22 +28,18 @@
       (is (= (response :status) 201))
       (is (re-matches #"/stories/\d+" ((response :headers) "Location")))))
   (testing "invalid empty"
-    (let [story {}
-          response (app (request :post "/stories" story))]
-      (is (= (response :status) 400))))
+    (test-r-create-invalid {}))
   (testing "invalid points non-number"
-    (let [story (assoc STORY_VALID :points "X")
-          response (app (request :post "/stories" story))]
-      (is (= (response :status) 400))))
+    (test-r-create-invalid (assoc STORY_VALID :points "X")))
   (testing "invalid priority non-integer"
-    (let [story (assoc STORY_VALID :priority 1.0)
-          response (app (request :post "/stories" story))]
-      (is (= (response :status) 400))))
+    (test-r-create-invalid (assoc STORY_VALID :priority 1.0)))
   (testing "invalid title missing"
-    (let [story (dissoc STORY_VALID :title)
-          response (app (request :post "/stories" story))]
-      (is (= (response :status) 400))))
-  )
+    (test-r-create-invalid (dissoc STORY_VALID :title))))
+
+(defn- test-r-read-not-found [location]
+  (let [response (app (request :get location))
+        body (json/parse-string (response :body) true)]
+    (is (= (response :status) 404))))
 
 (deftest test-r-read
   (testing "found"
@@ -49,15 +49,24 @@
           body (json/parse-string (response :body) true)]
       (is (= body story))))
   (testing "not-found integer"
-    (let [location "/stories/666666666"
-          response (app (request :get location))
-          body (json/parse-string (response :body) true)]
-      (is (= (response :status) 404))))
+    (test-r-read-not-found "/stories/666666666"))
   (testing "not-found non-integer"
-    (let [location "/stories/X"
-          response (app (request :get location))
-          body (json/parse-string (response :body) true)]
-      (is (= (response :status) 404)))))
+    (test-r-read-not-found "/stories/X")))
+
+(defn- test-r-update-invalid [story2]
+  (let [story STORY_VALID
+        location (seed-story story)
+        response (app (request :put location story2))]
+    (is (= (response :status) 400))
+    (let [response2 (app (request :get location))
+          body2 (json/parse-string (response2 :body) true)]
+      (is (= body2 story)))))
+
+(defn- test-r-update-not-found [location]
+  (let [story STORY_VALID
+        response (app (request :put location story))
+        body (json/parse-string (response :body) true)]
+    (is (= (response :status) 404))))
 
 (deftest test-r-update
   (testing "valid"
@@ -70,54 +79,21 @@
             body2 (json/parse-string (response2 :body) true)]
         (is (= body2 story2)))))
   (testing "not-found integer"
-    (let [story STORY_VALID
-          location "/stories/666666666"
-          response (app (request :put location story))
-          body (json/parse-string (response :body) true)]
-      (is (= (response :status) 404))))
+    (test-r-update-not-found "/stories/666666666"))
   (testing "not-found non-integer"
-    (let [story STORY_VALID
-          location "/stories/X"
-          response (app (request :put location story))
-          body (json/parse-string (response :body) true)]
-      (is (= (response :status) 404))))
+    (test-r-update-not-found "/stories/X"))
   (testing "invalid empty"
-    (let [story STORY_VALID
-          story2 {}
-          location (seed-story story)
-          response (app (request :put location story2))]
-      (is (= (response :status) 400))
-      (let [response2 (app (request :get location))
-            body2 (json/parse-string (response2 :body) true)]
-        (is (= body2 story)))))
+    (test-r-update-invalid {}))
   (testing "invalid points non-number"
-    (let [story STORY_VALID
-          story2 (assoc STORY_VALID :points "X")
-          location (seed-story story)
-          response (app (request :put location story2))]
-      (is (= (response :status) 400))
-      (let [response2 (app (request :get location))
-            body2 (json/parse-string (response2 :body) true)]
-        (is (= body2 story)))))
+    (test-r-update-invalid (assoc STORY_VALID :points "X")))
   (testing "invalid priority non-integer"
-    (let [story STORY_VALID
-          story2 (assoc STORY_VALID :priority 1.0)
-          location (seed-story story)
-          response (app (request :put location story2))]
-      (is (= (response :status) 400))
-      (let [response2 (app (request :get location))
-            body2 (json/parse-string (response2 :body) true)]
-        (is (= body2 story)))))
+    (test-r-update-invalid (assoc STORY_VALID :priority 1.0)))
   (testing "invalid title missing"
-    (let [story STORY_VALID
-          story2 (dissoc STORY_VALID :title)
-          location (seed-story story)
-          response (app (request :put location story2))]
-      (is (= (response :status) 400))
-      (let [response2 (app (request :get location))
-            body2 (json/parse-string (response2 :body) true)]
-        (is (= body2 story)))))
-  )
+    (test-r-update-invalid (dissoc STORY_VALID :title))))
+
+(defn- test-r-delete-not-found [location]
+  (let [response (app (request :delete location))]
+    (is (= (response :status) 404))))
 
 (deftest test-r-delete
   (testing "found"
@@ -126,11 +102,6 @@
           response (app (request :delete location))]
       (is (= (response :status) 204))))
   (testing "not-found integer"
-    (let [location "/stories/666666666"
-          response (app (request :delete location))]
-      (is (= (response :status) 404))))
+    (test-r-delete-not-found "/stories/666666666"))
   (testing "not-found non-integer"
-    (let [location "/stories/X"
-          response (app (request :delete location))]
-      (is (= (response :status) 404))))
-  )
+    (test-r-delete-not-found "/stories/X")))
