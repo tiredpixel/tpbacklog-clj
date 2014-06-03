519697cdadb8bc22b2a94d44f30da1fd27a5c7ce
tiredpixel <tp@tiredpixel.com>
2014-06-03 13:45:53 +0100

    Use Lein >= 2.0.0 .

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject tpbacklog "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
                  [ring/ring-jetty-adapter "1.3.0-RC1"]
